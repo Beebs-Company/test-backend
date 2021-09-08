f33626f2ab4445139577e135a9b6fd7e6a7de849
PPRB <72214993+loic-beebs@users.noreply.github.com>
2021-09-08 14:23:58 +0200

    add data

--- src/carts.js
+++ src/carts.js
@@ -3,8 +3,10 @@ module.exports = {
     res.send({
       success: true,
       data: [
-        // 1 product
-        { id: 1, 
+        // (sum product)*(var fees) + (fixed fees) + (-wallet) + (shipping)
+        // 1 product => to pay (10)*(1.05) + (0.7) + (-0) + (0.99) = 12.19
+        { 
+          id: 1, 
           products: [
             {
               price: 10,
@@ -13,12 +15,15 @@ module.exports = {
           ],
           userId: 1,
         },
-        // 0 product
-        { id: 2, 
+        // 0 product => to pay 0
+        { 
+          id: 2, 
           products: [],
           userId: 1,
         },
-        { id: 3, 
+        // n product => to pay (10 + 5 + 10.2 + 1.25)*(1.05) + (0.7) + (-0) + (2.99) = 31.46 [31.4625]
+        { 
+          id: 3, 
           products: [
             {
               price: 10,
@@ -38,6 +43,109 @@ module.exports = {
             },
           ],
           userId: 1,
+        },
+        // n product + min free shipping => to pay (10 + 5 + 15 + 9.95 + 5.95)*(1.05) + (.7) + (-0) + (0) = 48.90 [48.89500000000001]
+        { 
+          id: 4, 
+          products: [
+            {
+              price: 10,
+              weight: 500,
+            },
+            {
+              price: 5,
+              weight: 200,
+            },
+            {
+              price: 15,
+              weight: 200,
+            },
+            {
+              price: 9.95,
+              weight: 50,
+            },
+            {
+              price: 5.95,
+              weight: 150,
+            },
+          ],
+          userId: 1,
+        },
+        // n product < free shipping + shipping credit => to pay (15 + 9.95 + 4.95)*(1.05) + (.7) + (-0) + (0) = 32.10 [32.095]
+        { 
+          id: 5, 
+          products: [
+            {
+              price: 15,
+              weight: 200,
+            },
+            {
+              price: 9.95,
+              weight: 50,
+            },
+            {
+              price: 4.95,
+              weight: 150,
+            },
+          ],
+          userId: 2,
+        },
+        // n product > free shipping + shipping credit
+        { 
+          id: 6, 
+          products: [
+            {
+              price: 15,
+              weight: 200,
+            },
+            {
+              price: 9.95,
+              weight: 50,
+            },
+            {
+              price: 4.95,
+              weight: 150,
+            },
+          ],
+          userId: 2,
+        },
+        // n product < free shipping + walletBalance => to pay (15 + 9.95 + 4.95)*(1.05) + (.7) + (-5.25) + (1.99) = 28.83 [28.834999999999997]
+        { 
+          id: 7, 
+          products: [
+            {
+              price: 15,
+              weight: 200,
+            },
+            {
+              price: 9.95,
+              weight: 50,
+            },
+            {
+              price: 4.95,
+              weight: 150,
+            },
+          ],
+          userId: 3,
+        },
+        // n product > free shipping + wallet balance => to pay (15 + 9.95 + 5.95)*(1.05) + (.7) + (-15.25) + (0) = 17.90 [17.895000000000003]
+        { 
+          id: 7, 
+          products: [
+            {
+              price: 15,
+              weight: 200,
+            },
+            {
+              price: 9.95,
+              weight: 50,
+            },
+            {
+              price: 5.95,
+              weight: 150,
+            },
+          ],
+          userId: 4,
         },
       ],
     });
--- src/params.js
+++ src/params.js
@@ -4,6 +4,8 @@ module.exports = {
       success: true, 
       data: {
         FREE_SHIPPING_MIN_AMOUNT: 30.0,
+        FIXED_BUYER_FEE: 0.7,
+        VARIABLE_BUYER_FEE: 5,
       }
     });
     resolve();
